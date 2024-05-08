fbdb3400610c58bc1d80f45b5c96b280add72952
Eleanor Bickell <148374096+E-Bickell@users.noreply.github.com>
2024-05-08 16:01:25 +0100

    FixedStreaks

--- tests/src/de/tomgrill/gdxtesting/utils/streakTests.java
+++ tests/src/de/tomgrill/gdxtesting/utils/streakTests.java
@@ -16,22 +16,22 @@ public class streakTests {
     MainGameScreen gameScreen=new MainGameScreen(game);
     @Test
     public void GymRat(){
-        ArrayList<String> gyms=new ArrayList<String>;
+        ArrayList<String> gyms=new ArrayList<String>();
         gyms.add('1');
-        gyms.add("Gym_door")
+        gyms.add("Gym_door");
         for (int i=0;i<10;i++){
             gameScreen.recActivtities.add(gyms);
         }
         gameScreen.dayNum=7;
-        assertEquals(true,gameScreen.streaks.contains('Gym Rat'));
+        assertEquals(true,gameScreen.streaks.contains("Gym Rat"));
 
     }
 
     @Test
     public void PiazzaStreak(){
-        ArrayList<String> gyms=new ArrayList<String>;
+        ArrayList<String> gyms=new ArrayList<String>();
         gyms.add('1');
-        gyms.add("Piazza_door")
+        gyms.add("Piazza_door");
         for (int i=0;i<10;i++){
             gameScreen.studyTimes.add(gyms);
         }
@@ -42,9 +42,9 @@ public class streakTests {
 
     @Test
     public void CompSciStreak(){
-        ArrayList<String> gyms=new ArrayList<String>;
+        ArrayList<String> gyms=new ArrayList<String>();
         gyms.add('1');
-        gyms.add("Comp_sci_door")
+        gyms.add("Comp_sci_door");
         for (int i=0;i<10;i++){
             gameScreen.studyTimes.add(gyms);
         }
